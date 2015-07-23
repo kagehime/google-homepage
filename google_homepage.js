a54d7864871f1d51fcd32029430bede31393a8ab
Candyce <cacollins1987@gmail.com>
2015-07-23 13:07:41 -0400

    Google Home page
    Made for Odin Project.

---
+++ google_homepage.js
@@ -0,0 +1,13 @@
+document.getElementById("first").addEventListener("click", Search);
+
+document.getElementById("luck").addEventListener("click", Lucky);
+
+ function Search(){
+ 	document.getElementById("first").value = "This isn't real";	
+};
+
+
+function Lucky(){
+ 	document.getElementById("luck").value = "Say What?";
+    };
+ 	
